eb02d8214248d093e6bc15a6fcc18eaf8ee76e70
Mikhail Melnyk <melnyk@melnyk-lx.nixsolutions.com>
2021-01-18 19:01:39 +0200

    Refactoring

--- task6/main.go
+++ task6/main.go
@@ -16,7 +16,6 @@ const (
 	commentsURL = "http://jsonplaceholder.typicode.com/comments?postId="
 )
 
-// Post struct
 type Post struct {
 	Id     int    `json:"id"`
 	UserId int    `json:"userId"`
@@ -24,26 +23,12 @@ type Post struct {
 	Body   string `json:"body"`
 }
 
-// Comment struct
 type Comment struct {
 	Id     int    `json:"id"`
 	PostId int    `json:"postId"`
 	Name   string `json:"name"`
 	Email  string `json:"email"`
 	Body   string `json:"body"`
-}
-
-func handleError(err *error) {
-	if *err != nil {
-		log.Println(*err)
-		return
-	}
-}
-
-func ErrorCheck(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
 }
 
 func dbConn() (db *sql.DB) {
@@ -58,51 +43,76 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-func insertPost(post *Post) {
-	db := dbConn()
+func insertPost(post *Post, db *sql.DB) {
 	insForm, err := db.Prepare("INSERT INTO posts(id, userId, title, body) VALUES(?,?,?,?)")
-	ErrorCheck(err)
-	_, e := insForm.Exec(post.Id, post.UserId, post.Title, post.Body)
-	ErrorCheck(e)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	_, err = insForm.Exec(post.Id, post.UserId, post.Title, post.Body)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 
-	defer db.Close()
 }
 
-func insertComment(comment *Comment) {
-	db := dbConn()
+func insertComment(comment *Comment, db *sql.DB) {
 	insForm, err := db.Prepare("INSERT INTO comments(id, postId, name, email, body) VALUES(?,?,?,?,?)")
-	ErrorCheck(err)
-	_, e := insForm.Exec(comment.Id, comment.PostId, comment.Name, comment.Email, comment.Body)
-	ErrorCheck(e)
-
-	defer db.Close()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	_, err = insForm.Exec(comment.Id, comment.PostId, comment.Name, comment.Email, comment.Body)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 
 }
 
 func main() {
+	db := dbConn()
+	defer db.Close()
 
 	response, err := http.Get(postsURL + strconv.Itoa(7))
-	handleError(&err)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
-	handleError(&err)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	var posts []Post
 	err = json.Unmarshal(body, &posts)
 
 	for _, post := range posts {
-		go insertPost(&post)
+		go insertPost(&post, db)
+
 		response2, err := http.Get(commentsURL + strconv.Itoa(post.Id))
-		handleError(&err)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
 		defer response2.Body.Close()
+
+		body, err := ioutil.ReadAll(response2.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
 		var comments []Comment
 		err = json.Unmarshal(body, &comments)
 		for _, comment := range comments {
-			go insertComment(&comment)
+			go insertComment(&comment, db)
 		}
 	}
 
